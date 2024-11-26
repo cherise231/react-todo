e3e72635b340714b1b5e36649d87009e74beb86a
cherise231 <cherise23.1@gmail.com>
2024-11-25 20:11:54 -0800

    lifting state and props handling

--- src/TodoListItem.jsx
+++ src/TodoListItem.jsx
@@ -4,11 +4,11 @@ import React from 'react';
 // I added ({ todo }) as a parameter in the TodoListItem function
 const TodoListItem = ({ todo }) => {
     return (
-        // <div>
+        
         // Update the todo object reference to come from props
         // I updated the reference to use todo.title in TodoListItem.
-        <li>{todo.title}</li>
-        // </div>
+        <li className="todo-list-item">{todo.title}</li>
+        
     );
 
 };
