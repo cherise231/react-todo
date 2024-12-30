f167a2aef8165ac36e6f729f04fb2ec15435e941
cherise231 <cherise23.1@gmail.com>
2024-12-29 18:43:36 -0800

    Created async data fetching and loading state to simulate the retrieval of data from an API.

--- src/App.jsx
+++ src/App.jsx
@@ -7,29 +7,42 @@ import "./App.css";
 // import todoListImage from "./assets/to-do-list.png"
 import todoListImage1 from "./assets/todo-list1.png";
 
-// creates custom hook to manage the state of the todo list, and retrieving todo list from local storage and for reusability
-const useSemiPersistentState = () => {
-  //creates a state var todoList to hold todo items and uses the function setTodoList to update todoList
-  //updates the default state to parse the value of the "savedTodoList" item
-  const [todoList, setTodoList] = useState(() => {
-    //this retrieves the value of "savedTodoList" from storage. JSON.parse takes the string and converts it to a javaScript obj or arr.
-    const savedTodoList = JSON.parse(localStorage.getItem("savedTodoList"));
-    //returns an empty array if savedTodoList is null
-    return savedTodoList || [];
-  });
+function App() {
+  //Initializes the todoList state to an empty array
+  const [todoList, setTodoList] = useState([]);
+
+  // const [todoList, setTodoList] = useState(()=> {
+  //   const savedTodoList = JSON.parse(localStorage.getItem("savedTodoList"));
+  //   return savedTodoList || [];
+  // });
+
+  //state to track loading status
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    //new Promise to simulate fetching data from an API
+    new Promise((resolve, reject) => {
+      setTimeout(() => {
+        const savedTodoList =
+          JSON.parse(localStorage.getItem("savedTodoList")) || [];
+        resolve({ data: { todoList: savedTodoList } });
+      }, 2000); //Delay 2 seconds
+    }).then((result) => {
+      //Updates the todoList state with the fetched data
+      setTodoList(result.data.todoList);
+      //Sets the loading status to false after the data is fetched
+      setIsLoading(false);
+    });
+  }, []); //Empty dependency array to run only once on mount
 
   // Defines a useEffect React hook with todoList as a dependency. The useEffect hook performs side effects in function components. It is being used to synchronize the "todoList" state with "localStorage".
   useEffect(() => {
-    localStorage.setItem("savedTodoList", JSON.stringify(todoList));
+    //checks if the data is not loading
+    if (isLoading === false) {
+      //will only update localStorage if the data is not loading
+      localStorage.setItem("savedTodoList", JSON.stringify(todoList));
+    }
   }, [todoList]);
-
-  //returns the state var "todoList" and setter function "setTodoList" as an array
-  return [todoList, setTodoList];
-};
-
-function App() {
-  //Updates App function to use the useSemiPersistentState hook
-  const [todoList, setTodoList] = useSemiPersistentState();
 
   //function to add a new todo
   function addTodo(newTodo) {
@@ -48,15 +61,26 @@ function App() {
 
   return (
     <>
-      <img src={todoListImage1} className="todo-list-image1" alt="Todo List" />
-      <h1>Todo List</h1>
+      {/* Adding a ternary operator to render loading message only if isLoading is true, otherwise it renders the TodoList component */}
+      {isLoading ? (
+        <p>Loading...</p>
+      ) : (
+        <>
+          <img
+            src={todoListImage1}
+            className="todo-list-image1"
+            alt="Todo List"
+          />
+          <h1>Todo List</h1>
 
-      {/*passes addTodo as a callback handler prop named onAddTodo. This allows AddTodoForm to call addTodo when a new todo is added*/}
-      <AddTodoForm onAddTodo={addTodo} />
+          {/*passes addTodo as a callback handler prop named onAddTodo. This allows AddTodoForm to call addTodo when a new todo is added*/}
+          <AddTodoForm onAddTodo={addTodo} />
 
-      {/*passes todoList state as a prop named todoList to the TodoList component */}
-      {/* passes removeTodo prop as a callback handler prop called onRemoveTodo to the TodoList component */}
-      <TodoList todoList={todoList} onRemoveTodo={removeTodo} />
+          {/*passes todoList state as a prop named todoList to the TodoList component */}
+          {/* passes removeTodo prop as a callback handler prop called onRemoveTodo to the TodoList component */}
+          <TodoList todoList={todoList} onRemoveTodo={removeTodo} />
+        </>
+      )}
     </>
   );
 }
